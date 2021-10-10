c0cf720c9fa32eb3ca483b0b25cca47f51d5b7f9
Jason <187157249@qq.com>
2021-10-10 20:41:09 +0800

    111

---
+++ src/test/java/com/pawpaw/common/BrowserUtilTest.java
@@ -0,0 +1,20 @@
+package com.pawpaw.common;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.pawpaw.common.util.BrowserUtil;
+import org.junit.Test;
+
+public class BrowserUtilTest {
+
+
+    @Test
+    public void openBrownse() throws JsonProcessingException {
+        BrowserUtil.openBrownse("http://www.baidu.com");
+    }
+
+
+}
+
+
+
+
---
+++ src/test/java/com/pawpaw/common/RestTemplateUtilTest.java
@@ -0,0 +1,31 @@
+package com.pawpaw.common;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.pawpaw.common.util.RestTemplateUtil;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class RestTemplateUtilTest {
+
+
+    @Test
+    public void test() throws JsonProcessingException {
+        String s = "http://abc.com/xxx?";
+        List<String> p = new LinkedList<>();
+        p.add("a");
+        p.add("b");
+        p.add("c");
+        String url = RestTemplateUtil.appendParam(s, p);
+        System.out.println(url);
+        url = RestTemplateUtil.buildRestTemplateUrl("abc.com", 443, "xxx?", p);
+        System.out.println(url);
+        url = RestTemplateUtil.buildRestTemplateUrl("http","abc.com", 80, "xxx?", p);
+        System.out.println(url);
+    }
+}
+
+
+
+
