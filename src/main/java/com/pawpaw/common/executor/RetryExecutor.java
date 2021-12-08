2a4dc33a92d682f8798fbfd70e030f82fa6d02ff
Jason <187157249@qq.com>
2021-12-08 19:19:49 +0800

    1

--- src/main/java/com/pawpaw/common/executor/RetryExecutor.java
+++ src/main/java/com/pawpaw/common/executor/RetryExecutor.java
@@ -1,6 +1,8 @@
-package com.pawpaw.common.concurrent;
+package com.pawpaw.common.executor;
 
+import com.pawpaw.common.executor.call.ReturnableExecutorCall;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.Callable;
 
@@ -9,24 +11,31 @@ import java.util.concurrent.Callable;
  * 重试maxTryTime次。一共执行maxTryTime+1次
  */
 @AllArgsConstructor
+@Slf4j
 public class RetryExecutor {
     private final long maxTryTime;
 
-    public <T> T execute(Callable<T> call) {
+    public <T> T execute(ReturnableExecutorCall<T> call) {
 
         for (int retryTime = 0; retryTime <= this.maxTryTime; retryTime++) {
             try {
-                T t = call.call();
+                call.run();
+                T t = call.getReturn();
                 return t;
             } catch (Throwable e) {
                 if (retryTime >= this.maxTryTime) {
                     throw new RuntimeException(e);
+                } else {
+                    log.error("跳过此异常:{},当前重试次数:{},最大重试次数:{}", e.getMessage(), retryTime, maxTryTime);
+                    continue;
                 }
-                continue;
+
 
             }
 
         }
+        //never reach here
+        //never reach here
         throw new RuntimeException();
     }
 
--- src/test/java/com/pawpaw/common/RetryExecutorTest.java
+++ src/test/java/com/pawpaw/common/RetryExecutorTest.java
@@ -1,7 +1,7 @@
 package com.pawpaw.common;
 
-import com.pawpaw.common.concurrent.RetryExecutor;
-import com.pawpaw.common.util.ArrayUtil;
+import com.pawpaw.common.executor.RetryExecutor;
+import com.pawpaw.common.executor.call.ReturnableExecutorCall;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,24 +9,36 @@ import org.junit.Test;
 import java.util.concurrent.Callable;
 
 public class RetryExecutorTest {
+    ThreadLocal<Integer> tl = new ThreadLocal();
+
+    @Before
+    public void init() {
+        tl.set(0);
+    }
+
+
+    ReturnableExecutorCall call = new ReturnableExecutorCall<Object>() {
+        @Override
+        public void call() {
+            Integer num = tl.get();
+            tl.set(++num);
+            throw new RuntimeException("test");
+        }
+
+        @Override
+        public Object getReturn() {
+            return null;
+        }
+    };
 
 
     @Test
     public void execute() {
         RetryExecutor executor = new RetryExecutor(3);
-        ThreadLocal<Integer> tl = new ThreadLocal();
-        tl.set(0);
         try {
-            executor.execute(new Callable<Object>() {
-                @Override
-                public Object call() throws Exception {
-                    Integer num = tl.get();
-                    tl.set(++num);
-                    throw new RuntimeException("test");
-                }
-            });
+            executor.execute(call);
         } catch (Exception e) {
-
+            //不做任何操作，主要是防止最后一次运行call的时候test case 抛出异常被中断
         }
         Assert.assertTrue(tl.get() == 4);
     }
